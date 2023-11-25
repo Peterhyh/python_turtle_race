fbc86c36e89d10c8154a3f4c9d60732f56e060fe
Peter Huynh <peterhyh@yahoo.com>
2023-11-24 19:22:51 -0600

    turtles lined up at the starting position

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from turtle import Turtle, Screen
 
-racer = Turtle(shape="turtle")
+
 screen = Screen()
 screen.setup(width=500, height=400)
 
-color_arr = ["red", "blue", "green", "purple", "black", "pink",]
+color_arr = ["red", "blue", "green", "purple", "black", "pink"]
 
-racer.color(color_arr[3])
+y_coord = -70
+
+for color_index in range(0, 6):
+    racer = Turtle(shape="turtle")
+    racer.color(color_arr[color_index])
+    racer.penup()
+    racer.goto(x=-230, y=y_coord)
+    y_coord += 30
+
 
 screen.exitonclick()
