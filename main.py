6d6c13db1eb15667e592abcbb7221c074b7110dd
Peter Huynh <peterhyh@yahoo.com>
2023-11-27 11:54:36 -0600

    turtle race finish line complete

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random
 
 
 screen = Screen()
@@ -6,15 +7,36 @@ screen.setup(width=500, height=400)
 
 color_arr = ["red", "blue", "green", "purple", "black", "orange"]
 
+user_bet = screen.textinput(
+    title="Make your bet", prompt="Enter the color turtle that you think will win the race: ")
+
 y_coord = -70
 is_on = False
 
+all_turtles = []
+
 for color_index in range(0, 6):
-    racer = Turtle(shape="turtle")
-    racer.color(color_arr[color_index])
-    racer.penup()
-    racer.goto(x=-230, y=y_coord)
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.color(color_arr[color_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_coord)
     y_coord += 30
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_on = True
+
+while is_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_on = False
+            winning_color = turtle.pencolor()
+            if user_bet == winning_color:
+                print(f"You won! {winning_color} turtle won the race!")
+            else:
+                print(f"You lost! {winning_color} turtle won the race!")
+        distance = random.randint(0, 10)
+        turtle.fd(distance)
 
 
 screen.exitonclick()
